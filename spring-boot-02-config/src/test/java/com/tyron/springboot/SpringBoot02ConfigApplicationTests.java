57abcceb0ae5e780fd558235b026c56199cc5335
tyronczt <310674619@qq.com>
2019-01-22 00:59:54 +0800

    测试类

--- spring-boot-02-config/src/test/java/com/tyron/springboot/SpringBoot02ConfigApplicationTests.java
+++ spring-boot-02-config/src/test/java/com/tyron/springboot/SpringBoot02ConfigApplicationTests.java
@@ -1,7 +1,9 @@
 package com.tyron.springboot;
 
+import com.tyron.springboot.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringBoot02ConfigApplicationTests {
 
+    @Autowired
+    Person person;
+
     @Test
     public void contextLoads() {
+        System.out.println(person);
     }
 
 }
