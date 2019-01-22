22b16e161946db31a53cd136d14391a1bbef9afc
tyronczt <310674619@qq.com>
2019-01-23 00:29:49 +0800

    测试properties配置文件

--- spring-boot-02-config/src/main/java/com/tyron/springboot/bean/Person.java
+++ spring-boot-02-config/src/main/java/com/tyron/springboot/bean/Person.java
@@ -1,7 +1,10 @@
 package com.tyron.springboot.bean;
 
+import org.hibernate.validator.constraints.Email;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import java.util.Date;
 import java.util.List;
@@ -11,22 +14,36 @@ import java.util.Map;
  * @Description: 人员实体类
  * @Author: tyron
  * @date: 2019/1/22
- *
+ * <p>
  * 将配置文件中配置的每一个属性的值，映射到这个组件中
- * @ConfigurationProperties：告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定；
- *       prefix = "person"：配置文件中哪个下面的所有属性进行一一映射
- *
+ * @ConfigurationProperties：告诉SpringBoot将本类中的所有属性和配置文件中相关的配置进行绑定； prefix = "person"：配置文件中哪个下面的所有属性进行一一映射
+ * <p>
  * 只有这个组件是容器中的组件，才能容器提供的@ConfigurationProperties功能；
  */
 @Component
 @ConfigurationProperties(prefix = "person")
+@Validated
 public class Person {
 
+    /**
+     * <bean class="Person">
+     *      <property name="lastName" value="字面量/${key}从环境变量、配置文件中获取值/#{SpEL}"></property>
+     * <bean/>
+     */
+
+//    @Value("${person.name}")
+//    @Email
     private String name;
+//    @Value("#{11*3}")
     private Integer age;
+//    @Value("false")
     private Boolean boss;
+//    @Value("2010/01/01")
     private Date birth;
 
+    // 通过@Value的测试：Person{name='小马', age=33, boss=false, birth=Fri Jan 01 00:00:00 CST 2010, maps=null, lists=null, animal=null}
+
+    //    @Value("${person.maps}")---不支持这种写法（IllegalArgumentException）
     private Map<String, Object> maps;
     private List<Object> lists;
     private Animal animal;
