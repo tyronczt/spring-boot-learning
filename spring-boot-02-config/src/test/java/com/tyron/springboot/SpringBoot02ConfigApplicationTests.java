ec541e4aaba7c9e03f62a3b6cf7932baaed84a40
tyronczt <310674619@qq.com>
2019-01-29 00:37:57 +0800

    add test method

--- spring-boot-02-config/src/test/java/com/tyron/springboot/SpringBoot02ConfigApplicationTests.java
+++ spring-boot-02-config/src/test/java/com/tyron/springboot/SpringBoot02ConfigApplicationTests.java
@@ -1,6 +1,7 @@
 package com.tyron.springboot;
 
 import com.tyron.springboot.bean.Person;
+import org.springframework.context.ApplicationContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,15 @@ public class SpringBoot02ConfigApplicationTests {
 
     @Autowired
     Person person;
+
+    @Autowired
+    ApplicationContext ioc;
+
+    @Test
+    public void testService() {
+        boolean containsBean = ioc.containsBean("helloService02");
+        System.out.println("containsBean：" + containsBean);
+    }
 
     @Test
     public void contextLoads() {
