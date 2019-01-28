435a736f5e0038e062522cfe930b2013002b4496
tyronczt <310674619@qq.com>
2019-01-29 00:39:57 +0800

    modify config

--- spring-boot-02-config/src/main/java/com/tyron/springboot/SpringBoot02ConfigApplication.java
+++ spring-boot-02-config/src/main/java/com/tyron/springboot/SpringBoot02ConfigApplication.java
@@ -2,13 +2,14 @@ package com.tyron.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+//@ImportResource(value = "classpath:beans.xml")
 @SpringBootApplication
 public class SpringBoot02ConfigApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot02ConfigApplication.class, args);
     }
-
 }
 
