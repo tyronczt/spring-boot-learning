847c8c56be47a71786b757216080a7896e10cd7d
tyronczt <310674619@qq.com>
2019-01-30 23:52:20 +0800

    Initial config-project

---
+++ spring-boot-02-config-02/src/main/java/com/tyron/springboot/SpringBoot02Config02Application.java
@@ -0,0 +1,14 @@
+package com.tyron.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot02Config02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot02Config02Application.class, args);
+    }
+
+}
+
