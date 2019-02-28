c94da70f9e4a59cbe286afe8dba30d46752f8100
tyronczt <310674619@qq.com>
2019-02-28 23:26:23 +0800

    Initial spring-boot-web

---
+++ spring-boot-04-web-restfulcrud/src/main/java/com/tyron/springboot/SpringBoot04WebRestfulcrudApplication.java
@@ -0,0 +1,13 @@
+package com.tyron.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot04WebRestfulcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot04WebRestfulcrudApplication.class, args);
+    }
+
+}
