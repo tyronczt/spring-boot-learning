2a6aae65f3c4a7ae618ed9799a7cb558667cf7ae
tyronczt <310674619@qq.com>
2019-03-06 23:55:54 +0800

    update modifyEmp

--- spring-boot-04-web-restfulcrud/src/main/java/com/tyron/springboot/controller/EmployeeController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/tyron/springboot/controller/EmployeeController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
@@ -43,14 +45,14 @@ public class EmployeeController {
      * 添加页面
      *
      * @param model 页面显示参数
-     * @return 添加页
+     * @return 添加页面
      */
-    @GetMapping("/emp/add")
-    public String getAdd(Model model) {
+    @GetMapping("/emp")
+    public String addPage(Model model) {
         // 获取部门列表
         Collection<Department> departments = departmentDao.getDepartments();
         model.addAttribute("depts", departments);
-        return "emp/add";
+        return "emp/edit";
     }
 
     /**
@@ -60,9 +62,42 @@ public class EmployeeController {
      * @return 员工列表页
      */
     @PostMapping("/emp")
-    public String add(Employee employee) {
+    public String addEmp(Employee employee) {
         System.out.println(employee);
         // 添加
+        employeeDao.save(employee);
+
+        // redirect：重定向
+        // forward：转发
+        return "redirect:/emps";
+    }
+
+    /**
+     * 修改页面（同添加页面）
+     *
+     * @param model 页面显示参数
+     * @return 页面
+     */
+    @GetMapping("/emp/{id}")
+    public String modifyPage(@PathVariable("id") Integer id, Model model) {
+        // 获取部门列表
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        // 获取原信息
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp", employee);
+        return "emp/edit";
+    }
+
+    /**
+     * 修改操作
+     *
+     * @param employee 员工信息
+     * @return 员工列表页
+     */
+    @PutMapping("emp")
+    public String editEmp(Employee employee) {
+        System.out.println(employee);
         employeeDao.save(employee);
         return "redirect:/emps";
     }
